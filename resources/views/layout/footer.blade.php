becf70d2b3fd8fd83826086451dacfa7d0154f2c
Feronixer <oshie.coder96@gmail.com>
2025-08-26 20:50:11 +0800

    edit css

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footer-container">
+        <p class="mb-2">Valera Dental Clinic &copy; 2025. All Rights Reserved.</p>
+        <p>Designed with care for your smile.</p>
+        <div class="footer-icon">
+            <a href="#" class="footer-icon-item">
+                <i class="bi bi-facebook"></i>
+            </a>
+            <a href="#" class="footer-icon-item">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#" class="footer-icon-item">
+                <i class="bi bi-twitter-x"></i>
+            </a>
+        </div>
+    </div>
+</footer>
