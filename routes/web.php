099c60ffc67fa9533df7fe3aefa398b8c6b5de15
Mac Laga <mac.laga11@gmail.com>
2025-09-09 21:58:59 +0800

    layout change

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Authentication\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-<<<<<<< HEAD
 use App\Http\Controllers\Admin\AccountManagementController;
 use App\Http\Controllers\Admin\AppointmentController;
 use App\Http\Controllers\Admin\ContentManagementController;
@@ -12,13 +11,11 @@ use App\Http\Controllers\Admin\ToothTalkController;
 use App\Http\Controllers\Admin\NotificationController as AdminNotificationController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Patient\DashboardController as PatientDashboardController;
-=======
 use App\Http\Controllers\Patient\CalendarController;
-use App\Http\Controllers\Patient\ProfileController;
+use App\Http\Controllers\Patient\ProfileController as PatientProfileController;
 use App\Http\Controllers\Patient\PatientRecord;
 use App\Http\Controllers\Patient\AnnouncementController;
 use App\Http\Controllers\Staff\StaffDashboard;
->>>>>>> 231552f78f3a13328a285a038385c4702eb0ef82
 use Illuminate\Support\Facades\Route;
 
 
@@ -28,32 +25,6 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class,'login']);
 Route::get('/register', [AuthController::class,'showRegisterForm'])->name(name: 'register');
 Route::post('/register', [AuthController::class,'register']);
-<<<<<<< HEAD
-Route::get('/patient/dashboard', [PatientDashboardController::class,'index'])->middleware('auth')->name('patient-dashboard');
-
-Route::middleware(['auth'])->group(function(){
-    //Admin Routes
-    Route::get('/admin/dashboard', [AdminDashboardController::class,'index'])->name('admin-dashboard');
-
-    Route::get('/admin/account-management', [AccountManagementController::class,'index'])->name('admin-account-management');
-    Route::post('/admin/account-management', [AccountManagementController::class,'store']);
-    Route::put('/admin/account-management/users/{user}', [AccountManagementController::class,'update'])->name('users.update');
-    Route::get('/admin/account-management/users/{id}', [AccountManagementController::class, 'show'])->name('users.show');
-    Route::delete('/admin/account-management/users/{id}', [AccountManagementController::class, 'destroy'])->name('users.delete');
-    Route::post('/admin/account-management/users/change-password/{id}', [AccountManagementController::class,'changePasswword']);
-
-
-    Route::get('/admin/appointment', [AppointmentController::class,'index'])->name('admin-appointment');
-    Route::get('/admin/content-management', [ContentManagementController::class,'index'])->name('admin-content-management');
-    Route::get('/admin/post-procedural', [PostProceduralController::class,'index'])->name('admin-post-procedural');
-    Route::get('/admin/toothtalk', [ToothTalkController::class,'index'])->name('admin-toothtalk');
-    Route::get('/admin/notifications', [AdminNotificationController::class,'index'])->name('admin-notification');
-    Route::get('/admin/profile', [ProfileController::class,'index'])->name('admin-profile');
-
-    //Logout Route
-    Route::post('/logout', [AuthController::class,'logout'])->name('logout');
-});
-=======
 
 Route::get('/admin/dashboard', [AdminDashboardController::class,'index'])->name('admin-dashboard');
 Route::get('/dashboard', function () {return view('dashboard');});
@@ -62,4 +33,3 @@ Route::get('/patient/profile', [ProfileController::class, 'index'])->name('patie
 Route::get('/patient/record', [PatientRecord::class, 'index'])->name('patient-record');
 Route::get('/patient/announcement', [AnnouncementController::class, 'index'])->name('patient-announcement');
 Route::get('/staff/dashboard',[StaffDashboard::class, 'index'])->name('staff-dashboard');
->>>>>>> 231552f78f3a13328a285a038385c4702eb0ef82
