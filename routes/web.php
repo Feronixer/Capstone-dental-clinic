13ccaf044345410182d5945e55cad837527ad590
Mac Laga <mac.laga11@gmail.com>
2025-09-09 22:11:48 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,29 @@ Route::post('/login', [AuthController::class,'login']);
 Route::get('/register', [AuthController::class,'showRegisterForm'])->name(name: 'register');
 Route::post('/register', [AuthController::class,'register']);
 
-Route::get('/admin/dashboard', [AdminDashboardController::class,'index'])->name('admin-dashboard');
-Route::get('/dashboard', function () {return view('dashboard');});
+Route::middleware(['auth'])->group(function(){
+    //Admin Routes
+    Route::get('/admin/dashboard', [AdminDashboardController::class,'index'])->name('admin-dashboard');
+
+    Route::get('/admin/account-management', [AccountManagementController::class,'index'])->name('admin-account-management');
+    Route::post('/admin/account-management', [AccountManagementController::class,'store']);
+    Route::put('/admin/account-management/users/{user}', [AccountManagementController::class,'update'])->name('users.update');
+    Route::get('/admin/account-management/users/{id}', [AccountManagementController::class, 'show'])->name('users.show');
+    Route::delete('/admin/account-management/users/{id}', [AccountManagementController::class, 'destroy'])->name('users.delete');
+    Route::post('/admin/account-management/users/change-password/{id}', [AccountManagementController::class,'changePasswword']);
+
+
+    Route::get('/admin/appointment', [AppointmentController::class,'index'])->name('admin-appointment');
+    Route::get('/admin/content-management', [ContentManagementController::class,'index'])->name('admin-content-management');
+    Route::get('/admin/post-procedural', [PostProceduralController::class,'index'])->name('admin-post-procedural');
+    Route::get('/admin/toothtalk', [ToothTalkController::class,'index'])->name('admin-toothtalk');
+    Route::get('/admin/notifications', [AdminNotificationController::class,'index'])->name('admin-notification');
+    Route::get('/admin/profile', [ProfileController::class,'index'])->name('admin-profile');
+
+    //Logout Route
+    Route::post('/logout', [AuthController::class,'logout'])->name('logout');
+});
+
 Route::get('/patient/calendar', [CalendarController::class, 'index'])->name('patient-calendar'  );
 Route::get('/patient/profile', [ProfileController::class, 'index'])->name('patient-profile');
 Route::get('/patient/record', [PatientRecord::class, 'index'])->name('patient-record');
